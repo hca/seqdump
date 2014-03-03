13bc40c74873ad689b837f8dabd5967c9e521444
Héctor Condori Alagón <hector.codori.alagon@gmail.com>
2014-03-03 22:31:44 +0100

    yet another initial commit

--- src/seqdump.c
+++ src/seqdump.c
@@ -1,3 +1,7 @@
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,8 +12,12 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <string.h>
 
-#define PROGRAM_NAME "biotranslator"
+#include "safe-read.h"
+#include "full-write.h"
+
+#define PROGRAM_NAME "seqdump"
 
 #define GETOPT_HELP_CHAR  (CHAR_MIN - 2)
 #define GETOPT_VERSION_CHAR (CHAR_MIN - 3)
@@ -29,14 +37,16 @@
   case GETOPT_HELP_CHAR:                        \
   usage(EXIT_SUCCESS);                       \
   break;
+
   
 #define STREQ(s1, s2) ((strcmp (s1, s2) == 0))
 
-char* program_name = "biotranslator";
+char* program_name = "seqdump";
 
 static char const *infile;
 
 int header_flag=0;
+static int input_descriptor;
 
 static struct option long_options[] =
   {
@@ -48,9 +58,6 @@ static struct option long_options[] =
     { 0, 0, 0, 0 }
   };
 
-
-
-
 void
 usage(int status)
 {
@@ -58,8 +65,87 @@ usage(int status)
   exit(status);
 }
 
+void print_version()
+{
+  puts("seqdump 1.0");
+}
 
+void parse_fasta(char[] in_buffer, 
+  char[] out_buffer,
+  int *length,
+  int *in_pos,
+  int *out_pos,
+  int *state)
+{
+  while(*in_pos<*legth && *out_pos<*legth)
+    {
+      switch(in_buffer[(*in_pos)++])
+        {
+          case '\n':
+            if(*state==10)
+              *state=20;
+            else
+              *state=0;
+            break;
+          case ' ':
+          case '\t':
+            *state=1;
+            break;
+          case '>':
+            if(*state!=0)
+              {
+                state=-1;
+                return;
+              }
+            break;
+          case else:
+            /*TODO additional checks and optional change case*/
+            out_buffer[(*out_pos)++]==in_buffer[*in_pos];
+        }
+    }
+}
 
+bool dump(char *buf, size_t bufsize)
+{
+  size_t n_read;
+  while (true)
+    {
+      n_read = safe_read (input_descriptor, buf, bufsize);
+      
+      if (n_read == SAFE_READ_ERROR)
+        {
+          error (0, errno, "%s", infile);
+          return false;
+        }
+      
+      /*write the last block*/
+      if (n_read == 0)
+        if (full_write (STDOUT_FILENO, buf, n) != n)
+          error(EXIT_FAILURE, errno, "write error");
+        return true;
+
+      {
+        size_t n = n_read;
+        if(n_read == bufsize)
+          {
+          if (full_write (STDOUT_FILENO, buf, n) != n)
+            error(EXIT_FAILURE, errno, "write error");
+          }
+        else
+          {
+            ;
+          }        
+      }
+    }
+}
+
+static inline void *
+ptr_align (void const *ptr, size_t alignment)
+{
+  char const *p0 = ptr;
+  char const *p1 = p0 + alignment - 1;
+  return (void *) (p1 - (size_t) p1 % alignment);
+}
 
 int
 main(int argc, char *argv[])
@@ -81,6 +167,10 @@ main(int argc, char *argv[])
           break;
 
     case_GETOPT_HELP_CHAR;
+    
+        case GETOPT_VERSION_CHAR:
+          print_version();
+          return EXIT_SUCCESS;
 
         default:
           usage(EXIT_FAILURE);
@@ -90,7 +180,7 @@ main(int argc, char *argv[])
   
   int argind = optind;
   bool have_read_stdin = false;
-  static int input_desc;
+  
   int file_open_mode = O_RDONLY;
   
   do
@@ -101,15 +191,15 @@ main(int argc, char *argv[])
       if (STREQ (infile, "-"))
       {
         have_read_stdin = true;
-        input_desc = STDIN_FILENO;
+        input_descriptor = STDIN_FILENO;
         if ((file_open_mode) && !isatty (STDIN_FILENO))
           //open (stdin, "rb");
           ;
       }
       else
       {
-        input_desc = open (infile, file_open_mode);
-        if (input_desc < 0)
+        input_descriptor = open (infile, file_open_mode);
+        if (input_descriptor < 0)
         {
           error (0, errno, "%s", infile);
           ok = false;
